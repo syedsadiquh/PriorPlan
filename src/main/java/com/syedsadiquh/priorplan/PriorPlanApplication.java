591b9df354b7a4cb3444cad2a038ea6ed11e00e2
syedsadiquh <syedsadiquh@gmail.com>
2025-04-22 20:21:43 +0530

    Added: Spring Boot and Data JPA

---
+++ src/main/java/com/syedsadiquh/priorplan/PriorPlanApplication.java
@@ -0,0 +1,13 @@
+package com.syedsadiquh.priorplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriorPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriorPlanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/syedsadiquh/priorplan/PriorPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syedsadiquh.priorplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriorPlanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

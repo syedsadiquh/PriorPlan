3054bd94cd3dacee6e1bd6d023b87e28d4284022
syedsadiquh <syedsadiquh@gmail.com>
2025-04-23 18:08:32 +0530

    Added: Refactoring for running the program

--- src/main/java/com/syedsadiquh/priorplan/PriorPlanApplication.java
+++ src/main/java/com/syedsadiquh/priorplan/PriorPlanApplication.java
@@ -1,13 +1,26 @@
 package com.syedsadiquh.priorplan;
 
+import com.syedsadiquh.priorplan.ui.LoginSignupGUI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
 
 @SpringBootApplication
 public class PriorPlanApplication {
 
 	public static void main(String[] args) {
+
+		System.setProperty("java.awt.headless", "false");
+
 		SpringApplication.run(PriorPlanApplication.class, args);
+
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new LoginSignupGUI().setVisible(true);
+			}
+		});
 	}
 
 }
